859873c846a12562f3dd4a6b11bcc6f43c4bc68b
Christopher Maldonado <chmald@microsoft.com>
2017-08-08 10:21:52 -0500

    updates

--- staticsite.rb
+++ staticsite.rb
@@ -1,1 +1,14 @@
-puts 'Hello World from Ruby!'
+require 'webrick'
+options = {
+    :Port => ENV["PORT"].to_i,
+    :DirectoryIndex => ["hostingstart.html"],
+    :DocumentRoot => '.'
+}
+server = WEBrick::HTTPServer.new(options)
+server.mount_proc '/' do |req, res|
+    res.body = 'Hello from RUBY!'
+end
+
+trap 'INT' do server.shutdown end
+
+server.start
